6caa4396cc242ab52bff0635e4fcd8108f6be2b2
Bradley Holt <bradley.holt@foundline.com>
2010-01-17 13:55:08 -0500

    Create Entry Test.

---
+++ tests/application/models/EntryTest.php
@@ -0,0 +1,15 @@
+<?php
+
+class Postr_Model_EntryTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        /* Set Up Routine */
+    }
+
+    public function tearDown()
+    {
+        /* Tear Down Routine */
+    }
+}
+
