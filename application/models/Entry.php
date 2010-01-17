81181dd328aa3fa5284a9742855763f4d4b500f0
Bradley Holt <bradley.holt@foundline.com>
2010-01-17 14:08:22 -0500

    Test and implement Entry Model.

--- application/models/Entry.php
+++ application/models/Entry.php
@@ -2,7 +2,178 @@
 
 class Postr_Model_Entry
 {
+    /**
+     * @var integer
+     */
+    private $_id;
 
+    /**
+     * @var string
+     */
+    private $_title;
 
+    /**
+     * @var string
+     */
+    private $_content;
+
+    /**
+     * @var string
+     */
+    private $_summary;
+
+    /**
+     * @var Zend_Date
+     */
+    private $_updated;
+
+    /**
+     * @var Zend_Date
+     */
+    private $_published;
+
+    /**
+     * Get ID
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->_id;
+    }
+
+    /**
+     * Set ID
+     *
+     * @param integer $value
+     * @return Postr_Model_Entry    Provides a fluent interface
+     */
+    public function setId($value)
+    {
+        $this->_id = (integer) $value;
+        return $this;
+    }
+
+    /**
+     * Get Title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->_title;
+    }
+
+    /**
+     * Set Title
+     *
+     * @param string $value
+     * @return Postr_Model_Entry    Provides a fluent interface
+     */
+    public function setTitle($value)
+    {
+        $this->_title = (string) $value;
+        return $this;
+    }
+
+    /**
+     * Get Content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->_content;
+    }
+
+    /**
+     * Set Content
+     *
+     * @param string $value
+     * @return Postr_Model_Entry    Provides a fluent interface
+     */
+    public function setContent($value)
+    {
+        $this->_content = (string) $value;
+        return $this;
+    }
+
+    /**
+     * Get Summary
+     *
+     * @return string
+     */
+    public function getSummary()
+    {
+        return $this->_summary;
+    }
+
+    /**
+     * Set Summary
+     *
+     * @param string $value
+     * @return Postr_Model_Entry    Provides a fluent interface
+     */
+    public function setSummary($value)
+    {
+        $this->_summary = (string) $value;
+        return $this;
+    }
+
+    /**
+     * Get Updated
+     * 
+     * The updated date object is treated as an immutable value object so a
+     * clone of the date object is returned to prevent it from being modified.
+     *
+     * @return Zend_Date
+     */
+    public function getUpdated()
+    {
+        return clone $this->_updated;
+    }
+
+    /**
+     * Set Updated
+     * 
+     * The updated date object is treated as an immutable value object so a
+     * clone of the date object is assigned to prevent it from being modified.
+     *
+     * @param Zend_Date $value
+     * @return Postr_Model_Entry    Provides a fluent interface
+     */
+    public function setUpdated(Zend_Date $value)
+    {
+        $this->_updated = clone $value;
+        return $this;
+    }
+
+    /**
+     * Get Published
+     * 
+     * The published date object is treated as an immutable value object so a
+     * clone of the date object is returned to prevent it from being modified.
+     *
+     * @return Zend_Date
+     */
+    public function getPublished()
+    {
+        return clone $this->_published;
+    }
+
+    /**
+     * Set Published
+     * 
+     * The published date object is treated as an immutable value object so a
+     * clone of the date object is assigned to prevent it from being modified.
+     *
+     * @param Zend_Date $value
+     * @return Postr_Model_Entry    Provides a fluent interface
+     */
+    public function setPublished(Zend_Date $value)
+    {
+        $this->_published = clone $value;
+        return $this;
+    }
 }
 
--- tests/application/models/EntryTest.php
+++ tests/application/models/EntryTest.php
@@ -16,5 +16,65 @@ class Postr_Model_EntryTest extends PHPUnit_Framework_TestCase
     {
         /* Tear Down Routine */
     }
+
+    public function testSetAndGetId()
+    {
+        $value = 371;
+        $this->_entry->setId($value);
+        $this->assertEquals(
+            $value,
+            $this->_entry->getId()
+        );
+    }
+
+    public function testSetAndGetTitle()
+    {
+        $value = 'Test Entry';
+        $this->_entry->setTitle($value);
+        $this->assertEquals(
+            $value,
+            $this->_entry->getTitle()
+        );
+    }
+
+    public function testSetAndGetContent()
+    {
+        $value = 'Test entry with' . PHP_EOL . 'multiple lines.';
+        $this->_entry->setContent($value);
+        $this->assertEquals(
+            $value,
+            $this->_entry->getContent()
+        );
+    }
+
+    public function testSetAndGetSummary()
+    {
+        $value = 'Test entry summary.';
+        $this->_entry->setSummary($value);
+        $this->assertEquals(
+            $value,
+            $this->_entry->getSummary()
+        );
+    }
+
+    public function testSetAndGetUpdated()
+    {
+        $value = new Zend_Date();
+        $this->_entry->setUpdated($value);
+        $this->assertEquals(
+            $value,
+            $this->_entry->getUpdated()
+        );
+    }
+
+    public function testSetAndGetPublished()
+    {
+        $value = new Zend_Date();
+        $this->_entry->setPublished($value);
+        $this->assertEquals(
+            $value,
+            $this->_entry->getPublished()
+        );
+    }
 }
 
