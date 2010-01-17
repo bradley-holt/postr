344a49ce0fc35c985e0740de450f93568b4ae7d0
Bradley Holt <bradley.holt@foundline.com>
2010-01-16 19:38:56 -0500

    Add bootstrapping code for application tests.

---
+++ tests/application/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+/** Zend_Application */
+require_once 'Zend/Application.php';
+
+// Create application, bootstrap, and run
+$application = new Zend_Application(
+    APPLICATION_ENV,
+    APPLICATION_PATH . '/configs/application.ini'
+);
+$application->bootstrap();
