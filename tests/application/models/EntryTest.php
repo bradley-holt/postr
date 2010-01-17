5d4a8a95d96303caf7d9e99b3e5291868c373eb4
Bradley Holt <bradley.holt@foundline.com>
2010-01-17 13:56:26 -0500

    Set up Entry Model in Entry Test.

--- tests/application/models/EntryTest.php
+++ tests/application/models/EntryTest.php
@@ -2,9 +2,14 @@
 
 class Postr_Model_EntryTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * @var Postr_Model_Entry
+     */
+    private $_entry;
+
     public function setUp()
     {
-        /* Set Up Routine */
+        $this->_entry = new Postr_Model_Entry();
     }
 
     public function tearDown()
