a125108d4e9ad82d2c385d4db9a3d2febeb158b9
Bradley Holt <bradley.holt@foundline.com>
2010-01-17 20:23:59 -0500

    Implement and test Entry Repository.

--- application/models/EntryRepository.php
+++ application/models/EntryRepository.php
@@ -2,7 +2,129 @@
 
 class Postr_Model_EntryRepository
 {
+    /**
+     * @var Zend_Db_Table_Abstract
+     */
+    protected $_entryTable;
 
+    protected function _populateRowFromEntry(
+        Postr_Model_Entry $entry,
+        Zend_Db_Table_Row_Abstract $entryRow = null
+    )
+    {
+        if (null === $entryRow) {
+            $entryRow = $this->_entryTable->createRow();
+        }
+        $entryRow->id = $entry->getId();
+        $entryRow->title = $entry->getTitle();
+        $entryRow->content = $entry->getContent();
+        $entryRow->summary = $entry->getSummary();
+        $entryRow->updated = $entry->getUpdated()->get(Zend_Date::ISO_8601);
+        $entryRow->published = $entry->getPublished()->get(Zend_Date::ISO_8601);
+        return $entryRow;
+    }
 
+    protected function _createEntryFromRow(Zend_Db_Table_Row_Abstract $entryRow)
+    {
+        $updated = new Zend_Date($entryRow->updated, Zend_Date::ISO_8601);
+        $published = new Zend_Date($entryRow->published, Zend_Date::ISO_8601);
+        $entry = new Postr_Model_Entry();
+        $entry
+            ->setId($entryRow->id)
+            ->setTitle($entryRow->title)
+            ->setContent($entryRow->content)
+            ->setSummary($entryRow->summary)
+            ->setUpdated($updated)
+            ->setPublished($published)
+        ;
+        return $entry;
+    }
+
+    public function __construct(Zend_Db_Table_Abstract $entryTable = null)
+    {
+        if (null === $entryTable) {
+            $entryTable = new Postr_Model_DbTable_Entry();
+        }
+        $this->_entryTable = $entryTable;
+    }
+
+    public function indexOfEntries()
+    {
+        $dbAdapter = $this->_entryTable->getAdapter();
+        $dbAdapter->beginTransaction();
+        $entryRowset = $this->_entryTable->fetchAll();
+        $entries = array();
+        foreach ($entryRowset as $entryRow) {
+            $entries[] = $this->_createEntryFromRow($entryRow);
+        }
+        $dbAdapter->commit();
+        return $entries;
+    }
+
+    /**
+     * Get Entry
+     *
+     * @param integer $id
+     * @return Postr_Model_Entry
+     */
+    public function getEntry($id)
+    {
+        $dbAdapter = $this->_entryTable->getAdapter();
+        $dbAdapter->beginTransaction();
+        $entryRow = $this->_entryTable->find($id)->current();
+        $dbAdapter->commit();
+        if (null === $entryRow) {
+            return null;
+        }
+        return $this->_createEntryFromRow($entryRow);
+    }
+
+    /**
+     * Post Entry
+     *
+     * @param Postr_Model_Entry $entry
+     * @return Postr_Model_EntryRepository      Provides a fluent interface
+     */
+    public function postEntry(Postr_Model_Entry $entry)
+    {
+        $dbAdapter = $this->_entryTable->getAdapter();
+        $dbAdapter->beginTransaction();
+        $entryRow = $this->_populateRowFromEntry($entry);
+        $entryRow->id = null;
+        $entryRow->save();
+        $entry->setId($dbAdapter->lastInsertId());
+        $dbAdapter->commit();
+        return $this;
+    }
+
+    /**
+     * Put Entry
+     *
+     * @param Postr_Model_Entry $entry
+     * @return Postr_Model_EntryRepository      Provides a fluent interface
+     */
+    public function putEntry(Postr_Model_Entry $entry)
+    {
+        $dbAdapter = $this->_entryTable->getAdapter();
+        $dbAdapter->beginTransaction();
+        $entryRow = $this->_entryTable->find($entry->getId())->current();
+        if (null === $entryRow) {
+            //TODO: Throw more specific exception
+            throw new Exception('Entry row must already exist.');
+        }
+        $this->_populateRowFromEntry($entry, $entryRow);
+        $entryRow->save();
+        $dbAdapter->commit();
+        return $this;
+    }
+
+    public function deleteEntry(Postr_Model_Entry $entry)
+    {
+        $dbAdapter = $this->_entryTable->getAdapter();
+        $dbAdapter->beginTransaction();
+        $entryRow = $this->_entryTable->find($entry->getId())->current();
+        $entryRow->delete();
+        $dbAdapter->commit();
+        return $this;
+    }
 }
-
--- tests/application/models/EntryRepositoryTest.php
+++ tests/application/models/EntryRepositoryTest.php
@@ -2,14 +2,105 @@
 
 class Postr_Model_EntryRepositoryTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * @var Postr_Model_Entry
+     */
+    private $_testEntry;
+
+    /**
+     * @var Postr_Model_EntryRepository
+     */
+    private $_entryRepository;
+
     public function setUp()
     {
-        /* Set Up Routine */
+        include APPLICATION_PATH . '/../scripts/load.sqlite.php';
+        $now = new Zend_Date();
+        $this->_testEntry = new Postr_Model_Entry();
+        $this->_testEntry
+            ->setTitle('Test Entry')
+            ->setContent('Test entry with' . PHP_EOL . 'multiple lines.')
+            ->setSummary('Test entry summary.')
+            ->setUpdated($now)
+            ->setPublished($now)
+        ;
+        $this->_entryRepository = new Postr_Model_EntryRepository();
+        parent::setUp();
     }
 
     public function tearDown()
     {
-        /* Tear Down Routine */
+        parent::tearDown();
+    }
+
+    public function testIndexOfEntriesCountIsThreeAfterPostingThreeEntries()
+    {
+        $this->_entryRepository->postEntry($this->_testEntry);
+        $this->_entryRepository->postEntry($this->_testEntry);
+        $this->_entryRepository->postEntry($this->_testEntry);
+        $entries = $this->_entryRepository->indexOfEntries();
+        $this->assertEquals(
+            3,
+            count($entries)
+        );
+    }
+
+    public function testGetEntryIsEqualToPostedEntry()
+    {
+        $this->_entryRepository->postEntry($this->_testEntry);
+        $this->assertTrue(
+            $this->_testEntry->isEqualTo(
+                $this->_entryRepository->getEntry($this->_testEntry->getId())
+            )
+        );
+    }
+
+    public function testEntryIdIsGreaterThanZeroAfterEntryIsPosted()
+    {
+        $this->_entryRepository->postEntry($this->_testEntry);
+        $this->assertGreaterThan(
+            0,
+            $this->_testEntry->getId()
+        );
+        return $this->_testEntry->getId();
+    }
+
+    public function testGetEntryIsEqualToPutEntry()
+    {
+        $this->_entryRepository->postEntry($this->_testEntry);
+        $now = new Zend_Date();
+        $now->addMonth(1);
+        $entry = $this->_entryRepository->getEntry($this->_testEntry->getId());
+        $entry
+            ->setTitle('Updated Test Entry')
+            ->setContent('Updated Test entry with' . PHP_EOL . 'multiple lines.')
+            ->setSummary('Updated Test entry summary.')
+            ->setUpdated($now)
+        ;
+        $this->_entryRepository->putEntry($entry);
+        $this->assertTrue(
+            $entry->isEqualTo(
+                $this->_entryRepository->getEntry($entry->getId())
+            )
+        );
+    }
+
+    /**
+     * @expectedException Exception
+     */
+    public function testPuttingNonExistentEntryThrowsException()
+    {
+        $this->_testEntry->setId(42);
+        $this->_entryRepository->putEntry($this->_testEntry);
+    }
+
+    public function testDeletedEntryIsNull()
+    {
+        $this->_entryRepository->postEntry($this->_testEntry);
+        $entry = $this->_entryRepository->getEntry($this->_testEntry->getId());
+        $this->_entryRepository->deleteEntry($entry);
+        $deletedEntry = $this->_entryRepository->getEntry($entry->getId());
+        $this->assertNull($deletedEntry);
     }
 }
 
