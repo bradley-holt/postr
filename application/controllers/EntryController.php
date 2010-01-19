9a628cceb1d5da8ffebfdf806d9b3bd1ab9a4723
Bradley Holt <bradley.holt@foundline.com>
2010-01-19 09:28:35 -0500

    Implement and test Entry Controller Index Action.

--- application/controllers/EntryController.php
+++ application/controllers/EntryController.php
@@ -2,15 +2,31 @@
 
 class EntryController extends Zend_Controller_Action
 {
+    /**
+     * @var Postr_Model_EntryRepository
+     */
+    private $_entryRepository;
 
+    /**
+     * Init
+     *
+     * @return void
+     */
     public function init()
     {
-        /* Initialize action controller here */
+        //TODO: Pass this in so that it can be substitued
+        $this->_entryRepository = new Postr_Model_EntryRepository();
     }
 
+    /**
+     * Index Action
+     *
+     * @return void
+     */
     public function indexAction()
     {
-        // action body
+        $entries = $this->_entryRepository->indexOfEntries();
+        $this->view->entries = $entries;
     }
 
     public function getAction()
