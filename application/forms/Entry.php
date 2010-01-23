213b7dc945589a4ccac43f1dfc29ff46a1099a8f
Bradley Holt <bradley.holt@foundline.com>
2010-01-23 12:29:52 -0500

    Implement and test Entry Form.

--- application/forms/Entry.php
+++ application/forms/Entry.php
@@ -5,7 +5,36 @@ class Postr_Form_Entry extends Zend_Form
 
     public function init()
     {
-        /* Form Elements & Other Definitions Here ... */
+        $titleElement = new Zend_Form_Element_Text('title');
+        $titleElement
+            ->setLabel('Title')
+        ;
+        $this->addElement($titleElement);
+        $contentElement = new Zend_Form_Element_Textarea('content');
+        $contentElement
+            ->setLabel('Content')
+        ;
+        $this->addElement($contentElement);
+        $summaryElement = new Zend_Form_Element_Textarea('summary');
+        $summaryElement
+            ->setLabel('Summary')
+        ;
+        $this->addElement($summaryElement);
+        $updatedElement = new Zend_Form_Element_Text('updated');
+        $updatedElement
+            ->setLabel('Updated')
+        ;
+        $this->addElement($updatedElement);
+        $publishedElement = new Zend_Form_Element_Text('published');
+        $publishedElement
+            ->setLabel('Published')
+        ;
+        $this->addElement($publishedElement);
+        $submitElement = new Zend_Form_Element_Submit('submit');
+        $submitElement
+            ->setLabel('Submit')
+        ;
+        $this->addElement($submitElement);
     }
 
 
---
+++ tests/application/forms/EntryTest.php
@@ -0,0 +1,129 @@
+<?php
+
+class Postr_Form_EntryTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Postr_Form_Entry
+     */
+    private $_entryForm;
+
+    public function setUp()
+    {
+        $this->_entryForm = new Postr_Form_Entry();
+        parent::setUp();
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+    }
+
+    public function testTitleIsATextElement()
+    {
+        $element = $this->_entryForm->getElement('title');
+        $this->assertType(
+            'Zend_Form_Element_Text',
+            $element
+        );
+    }
+
+    public function testTitleElementHasCorrectLabel()
+    {
+        $element = $this->_entryForm->getElement('title');
+        $this->assertEquals(
+            'Title',
+            $element->getLabel()
+        );
+    }
+
+    public function testContentIsATextareaElement()
+    {
+        $element = $this->_entryForm->getElement('content');
+        $this->assertType(
+            'Zend_Form_Element_Textarea',
+            $element
+        );
+    }
+
+    public function testContentElementHasCorrectLabel()
+    {
+        $element = $this->_entryForm->getElement('content');
+        $this->assertEquals(
+            'Content',
+            $element->getLabel()
+        );
+    }
+
+    public function testSummaryIsATextareaElement()
+    {
+        $element = $this->_entryForm->getElement('summary');
+        $this->assertType(
+            'Zend_Form_Element_Textarea',
+            $element
+        );
+    }
+
+    public function testSummaryElementHasCorrectLabel()
+    {
+        $element = $this->_entryForm->getElement('summary');
+        $this->assertEquals(
+            'Summary',
+            $element->getLabel()
+        );
+    }
+
+    public function testUpdatedIsATextElement()
+    {
+        $element = $this->_entryForm->getElement('updated');
+        $this->assertType(
+            'Zend_Form_Element_Text',
+            $element
+        );
+    }
+
+    public function testUpdatedElementHasCorrectLabel()
+    {
+        $element = $this->_entryForm->getElement('updated');
+        $this->assertEquals(
+            'Updated',
+            $element->getLabel()
+        );
+    }
+
+    public function testPublishedIsATextElement()
+    {
+        $element = $this->_entryForm->getElement('published');
+        $this->assertType(
+            'Zend_Form_Element_Text',
+            $element
+        );
+    }
+
+    public function testPublishedElementHasCorrectLabel()
+    {
+        $element = $this->_entryForm->getElement('published');
+        $this->assertEquals(
+            'Published',
+            $element->getLabel()
+        );
+    }
+
+    public function testSubmitIsASubmitElement()
+    {
+        $element = $this->_entryForm->getElement('submit');
+        $this->assertType(
+            'Zend_Form_Element_Submit',
+            $element
+        );
+    }
+
+    public function testSubmitElementHasCorrectLabel()
+    {
+        $element = $this->_entryForm->getElement('submit');
+        $this->assertEquals(
+            'Submit',
+            $element->getLabel()
+        );
+    }
+}
+
